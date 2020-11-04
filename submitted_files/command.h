2b4e44ee41f1f9bec2bc74356d30a2417fbef2c4
Duane Goodner <goodnedu@os1.engr.oregonstate.edu>
2020-11-03 22:45:58 -0800

    commit  immediately after submission

---
+++ submitted_files/command.h
@@ -0,0 +1,38 @@
+
+#include <stdbool.h>
+
+#define MAX_CLL 2048
+#define MAX_ARGS 512
+
+
+
+#ifndef THERE_IS_A_COMMAND
+#define THERE_IS_A_COMMAND
+
+struct command
+{
+    int arg_count;
+    char *args[MAX_ARGS];
+    char *input_redirect;
+    char *output_redirect;
+    bool background;
+    pid_t process_id;
+    struct command *next;
+};
+
+struct command *get_command(char* expand_wc, char* expand_repl);
+char* get_input_line(void);
+char** parse_input_line(char *curr_line, int *n_inputs);
+struct command *build_unexpanded_command(char** inputs, int *n_inputs);
+void get_argc_and_redirs(struct command* curr_command, char** inputs, int index_limit);
+void populate_args(struct command* curr_command, char** inputs);
+void expand_var(struct command* curr_command, char* old_str, char* new_str);
+
+bool is_comment(char** inputs);
+bool is_redirect_out(char* input);
+bool is_redirect_in(char* input);
+bool bg_command_check(char** inputs, int *n_inputs); 
+
+void free_command (struct command* curr_command);
+
+#endif
